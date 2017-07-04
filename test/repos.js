25c84858438d58f8eed4fdc389e81ec912623e96
Ciro S. Costa <ciro.costa@liferay.com>
2017-07-04 11:51:16 -0300

    Imrpoves repos test

--- test/repos.js
+++ test/repos.js
@@ -1,122 +1,21 @@
-var test = require('tap').test;
-var pushover = require('../');
+const fs = require('fs');
+const path = require('path');
+const http = require('http');
+const logger = require('debug')('pushover:test');
+const pushover = require('../');
 
-var fs = require('fs');
-var path = require('path');
-var tmpdir = require('os').tmpdir;
-var exists = fs.exists || path.exists;
+const {test} = require('tap');
+const {run, initDirs} = require('./test-util.js');
 
-var spawn = require('child_process').spawn;
-var exec = require('child_process').exec;
-var http = require('http');
+const dirs = initDirs();
+const port = Math.floor(Math.random() * ((1 << 16) - 1e4)) + 1e4;
 
-var seq = require('seq');
+test('create, push to, and clone a repo', async t => {
+  let lastCommit = '';
+  let firstTag = true;
 
-test('create, push to, and clone a repo', function(t) {
-  t.plan(17);
-
-  var repoDir = path.join(
-    tmpdir(),
-    Math.floor(Math.random() * (1 << 30)).toString(16)
-  );
-  var srcDir = path.join(
-    tmpdir(),
-    Math.floor(Math.random() * (1 << 30)).toString(16)
-  );
-  var dstDir = path.join(
-    tmpdir(),
-    Math.floor(Math.random() * (1 << 30)).toString(16)
-  );
-  var lastCommit;
-
-  fs.mkdirSync(repoDir, 0700);
-  fs.mkdirSync(srcDir, 0700);
-  fs.mkdirSync(dstDir, 0700);
-
-  var repos = pushover(repoDir, {autoCreate: false});
-  var port = Math.floor(Math.random() * ((1 << 16) - 1e4)) + 1e4;
-  var server = http.createServer(function(req, res) {
-    repos.handle(req, res);
-  });
-  server.listen(port, '127.0.0.1');
-
-  process.chdir(srcDir);
-  seq()
-    .seq(function() {
-      repos.create('doom', this);
-    })
-    .seq(function() {
-      var ps = spawn('git', ['init']);
-      ps.stderr.pipe(process.stderr, {end: false});
-      ps.on('exit', this.ok);
-    })
-    .seq(function() {
-      fs.writeFile(srcDir + '/a.txt', 'abcd', this);
-    })
-    .seq(function() {
-      spawn('git', ['add', 'a.txt']).on('exit', this.ok);
-    })
-    .seq_(function(next) {
-      var ps = spawn('git', ['commit', '-am', 'a!!']);
-      ps.on('exit', this.ok);
-      ps.stdout.pipe(process.stdout, {end: false});
-    })
-    .seq(function() {
-      var ps = spawn('git', ['tag', '0.0.1']);
-      ps.stderr.pipe(process.stderr, {end: false});
-      ps.on('exit', this.ok);
-    })
-    .seq(function() {
-      fs.writeFile(srcDir + '/a.txt', 'efgh', this);
-    })
-    .seq(function() {
-      spawn('git', ['add', 'a.txt']).on('exit', this.ok);
-    })
-    .seq_(function(next) {
-      var ps = spawn('git', ['commit', '-am', 'a!!']);
-      ps.on('exit', function() {
-        exec('git log | head -n1', function(err, stdout) {
-          lastCommit = stdout.split(/\s+/)[1];
-          next();
-        });
-      });
-      ps.stdout.pipe(process.stdout, {end: false});
-    })
-    .seq(function() {
-      var ps = spawn('git', ['tag', '0.0.2']);
-      ps.stderr.pipe(process.stderr, {end: false});
-      ps.on('exit', this.ok);
-    })
-    .seq(function() {
-      var ps = spawn('git', [
-        'push',
-        '--tags',
-        'http://127.0.0.1:' + port + '/doom.git',
-        'master',
-      ]);
-      ps.stderr.pipe(process.stderr, {end: false});
-      ps.on('exit', this.ok);
-    })
-    .seq(function() {
-      process.chdir(dstDir);
-      spawn('git', ['clone', 'http://127.0.0.1:' + port + '/doom.git']).on(
-        'exit',
-        this.ok
-      );
-    })
-    .seq_(function(next) {
-      exists(dstDir + '/doom/a.txt', function(ex) {
-        t.ok(ex, 'a.txt exists');
-        next();
-      });
-    })
-    .seq(function() {
-      server.close();
-      t.end();
-    })
-    .catch(t.fail);
-
-  repos.on('push', function(push) {
+  let repos = pushover(dirs.repo, {autoCreate: false});
+  repos.on('push', push => {
     t.equal(push.repo, 'doom.git', 'repo name');
     t.equal(push.commit, lastCommit, 'commit ok');
     t.equal(push.branch, 'master', 'master branch');
@@ -128,7 +27,6 @@ test('create, push to, and clone a repo', function(t) {
     push.accept();
   });
 
-  var firstTag = true;
   repos.on('tag', function(tag) {
     t.equal(tag.repo, 'doom.git', 'repo name');
     t.equal(tag.version, '0.0.' + (firstTag ? 1 : 2), 'tag received');
@@ -140,4 +38,36 @@ test('create, push to, and clone a repo', function(t) {
     tag.accept();
     firstTag = false;
   });
+
+  let server = http.createServer((req, res) => {
+    repos.handle(req, res);
+  });
+
+  server.listen(port, '127.0.0.1');
+  process.chdir(dirs.src);
+  repos.create('doom', this);
+
+  await run('git init');
+
+  fs.writeFileSync(`${dirs.src}/a.txt`, 'abcd');
+  await run('git add a.txt');
+  await run('git commit -am a!!');
+  await run('git tag 0.0.1');
+
+  fs.writeFileSync(`${dirs.src}/a.txt`, 'efgh');
+  await run('git add a.txt');
+  await run('git commit -am b!!');
+
+  let stdout = await run('git log | head -n1');
+  lastCommit = stdout.split(/\s+/)[1];
+  await run('git tag 0.0.2');
+
+  await run(`git push --tags http://127.0.0.1:${port}/doom.git master`);
+
+  process.chdir(dirs.dst);
+  await run(`git clone http://127.0.0.1:${port}/doom.git`);
+  t.ok(fs.existsSync(`${dirs.dst}/doom/a.txt`));
+
+  server.close();
+  t.end();
 });
