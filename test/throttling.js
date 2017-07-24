148a2123f06c034c18485a7b7ad79afaf96981e2
Ciro S. Costa <ciro.costa@liferay.com>
2017-07-24 16:20:32 -0300

    Adds option to attach a through stream to the POST request

---
+++ test/throttling.js
@@ -0,0 +1,40 @@
+const logger = require('debug')('gpusher:test');
+const gpusher = require('../');
+const fs = require('fs');
+const path = require('path');
+const http = require('http');
+
+const {tmpdir} = require('os');
+const {test} = require('tap');
+const {run, initDirs} = require('./test-util.js');
+
+const dirs = initDirs();
+const port = Math.floor(Math.random() * ((1 << 16) - 1e4)) + 1e4;
+
+test('subsequent pushes', async t => {
+  let repos = gpusher(dirs.repo, {
+    maxBytes: 1024,
+  });
+  let pushes = 0;
+
+  repos.on('push', push => {
+    pushes++;
+    push.accept();
+  });
+
+  let server = http.createServer(repos.handle.bind(repos));
+
+  server.listen(port, '127.0.0.1');
+  process.chdir(dirs.src);
+
+  await run(`git init`);
+  fs.writeFileSync(`${dirs.src}/a.txt`, 'abcd');
+  await run(`git add a.txt`);
+  await run(`git commit -am 'a!!'`);
+  await run(`git push http://127.0.0.1:${port}/doom master`);
+
+  t.equal(1, pushes);
+  server.close();
+
+  t.end();
+});
